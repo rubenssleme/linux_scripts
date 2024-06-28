772082e01e15d5e14c904bc5e3205f0dd396fec4
Rubens Leme <rubenss.leme@gmail.com>
2024-06-28 14:25:24 -0300

    Alteração no script com if para teste trinity_share e onedrive_share

---
+++ script_com_if.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+####################################
+#
+# Backup to NFS mount script.
+#
+####################################
+    
+# What to backup. 
+
+
+# Status de saída padrão da função trinityshare
+TrinityDIR=/media/trinity_share/files/
+OnedriveDIR=/media/onedrive_share/files/
+#find <$DIR> -type d -empty
+trinityshare() {
+  echo "Verificando Diretorio Trinity_share!"
+  ls -l $TrinityDIR > /dev/null
+}
+trinityshare
+if [ $? -ne 0 ]; then
+echo "Diretorio $TrinityDIR  vazio!"
+
+else
+echo "Ha conteudo no diretorio! $TrinityDIR "
+rclone --vfs-cache-mode  writes mount OneDrive: /media/onedrive_share &
+fi
+# Status de saída padrão da função onedriveshare
+onedriveshare() {
+  echo ""
+  echo "Verificando Diretorio Onedrive_shares!"
+  ls -l $OnedriveDIR > /dev/null
+}
+onedriveshare
+if [ $? -ne 0 ]; then
+echo ""
+echo "Diretorio $OnedriveDIR  vazio!"
+
+else
+echo ""
+echo "Ha conteudo no diretorio! $OnedriveDIR "
+#rclone --vfs-cache-mode  writes mount OneDrive: /media/onedrive_share &
+bash linux_scripts/trinity_t_rsync_to_onedrive_o.sh
+fi
